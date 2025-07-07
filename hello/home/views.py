6cb1ebd275f01f6c4c21a0adbe0fac4492c61ad5
On-cloud7 <priyanka.p62003@gmail.com>
2025-07-08 00:02:52 +0530

    files added

--- hello/home/views.py
+++ hello/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is home page")
+    return render(request,'index.html')
 
 def about(request):
     return HttpResponse("this is a about page")
